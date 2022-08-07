903bdec1932a9d64262d969d5009acb4e0330703
falvarenga <falvarenga@frba.utn.edu.ar>
2022-08-07 16:19:31 -0300

    login funcionando

--- src/app/generales/login/login.component.ts
+++ src/app/generales/login/login.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+type User = {
+  nombreDeUsuario: string | null;
+  password: string | null;
+};
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  user={
+    nombreDeUsuario: null,
+    password: null
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(user: User ) {
+      fetch('http://localhost:8080/usuarios/login', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        },
+
+        body: JSON.stringify(user)
+      }).then(response => response.json()) // te formatea el Json deja de ser solo en una linea
+      .then(data => this.guardar(data))
+      .catch(error => console.log(error));
+    }
+  guardar(idusuario : number){
+          localStorage.setItem('id',String(idusuario)); //localStorege.getItem('id')
+  }
 }
